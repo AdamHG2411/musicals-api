b8fc45fad9cb7e78d996a968464eced1455be449
Adam Grider <adam.h.grider@gmail.com>
2019-05-02 17:04:59 -0400

    final tweaks before going home

--- db/seed.js
+++ db/seed.js
@@ -6,18 +6,25 @@ const Venue = require('./models/Venue.js');
 const performances = require('./data/performances.json');
 const Performance = require('./models/Performance.js');
 
+//find all and delete
 Musical.find({}).deleteMany(() => {
 	Venue.find({}).deleteMany(() => {
 		Performance.find({}).deleteMany(() => {
+			//create all from json files
 			Musical.create(musicals).then(() => {
 				Venue.create(venues).then(() => {
 					Performance.create(performances).then(() => {
+						//find all performances
 						Performance.find({}).then((performances) => {
+							//for each performance
 							performances.forEach((thisPerformance) => {
+								//find matching musical and connect musicalId
 								Musical.findOne({ name: thisPerformance.musicalName }).then((musical) => {
 									Performance.updateOne(thisPerformance, { musicalId: musical._id }).then(() => {
+										//find matching venue and connect venueId
 										Venue.findOne({ code: thisPerformance.venue }).then((venue) => {
 											Performance.updateOne(thisPerformance, { venueId: venue._id }).then(() => {
+												//Save this performance
 												thisPerformance.save();
 											});
 										});

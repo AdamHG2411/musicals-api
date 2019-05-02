91444f30a4a7a39caf8937072c09362698628325
Adam Grider <adam.h.grider@gmail.com>
2019-05-02 10:25:18 -0400

    add CORS

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const musicalRouter = require('./routes/musicalRouter.js');
 const performanceRouter = require('./routes/performanceRouter.js');
 const venueRouter = require('./routes/venueRouter.js');
 
+app.use(cors());
+
 app.use(
 	parser.urlencoded({
 		extended: true
